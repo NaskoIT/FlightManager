37b2350601819be5c0a00bfbeeab848cd3f24c77
Atanas Vasilev <nasko01vasilev@gmail.com>
2020-02-27 18:44:42 +0200

    Scaffold Login and register pages, add reference to models project and use User instead IdentityUser in _LoginPartial.cshtml class

---
+++ FlightManager/FlightManager.Web/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using FlightManager.Data;
+using FlightManager.Models;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(FlightManager.Web.Areas.Identity.IdentityHostingStartup))]
+namespace FlightManager.Web.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
